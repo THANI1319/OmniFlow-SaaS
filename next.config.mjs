a0b2776893a9bcb187c869439d35ef4accae57ea
THANI1319 <2323059@saec.ac.in>
2026-09-15 20:31:03 +0530

    Fixed Vercel build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning irunthalum build fail aagathu
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Typescript error irunthalum bypass pannidum
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig; // (.mjs file ah iruntha 'export default nextConfig;' nu podunga)
+export default nextConfig;
